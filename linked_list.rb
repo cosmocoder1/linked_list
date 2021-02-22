051f6069847db2573b4d9e64537198bb477beccf
Nathan Lucy <cosmocoder1@gmail.com>
2021-02-22 11:20:13 -0600

    initial commit

---
+++ linked_list.rb
@@ -0,0 +1,18 @@
+class LinkedList
+
+  list = []
+
+  def append(value)
+    new_node = Node.new
+    new_node = 
+    list.push(Node.new)  
+  end  
+
+end
+
+class Node
+  attr_accessor :value, :next_node
+  initialize (value)
+  @value = nil
+  next_node = nil
+end  
