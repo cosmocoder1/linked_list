f5f9a163195c3520275a79f71c84716967c38807
Nathan Lucy <cosmocoder1@gmail.com>
2021-02-22 13:09:55 -0600

    further method dev

--- linked_list.rb
+++ linked_list.rb
@@ -5,8 +5,57 @@ class LinkedList
 
   def append(value)
     new_node = Node.new(value)
-    $list.push(new_node.value)  
+    $list.push(new_node)  
   end  
+
+  def prepend(value)
+    new_node = Node.new(value)
+    $list.unshift(new_node)
+  end  
+
+  def size
+    $list.count
+  end  
+
+  def head
+    $list[0]
+  end  
+
+  def tail
+    $list[$list.length - 1]
+  end  
+
+  def at (index)
+    $list[index]
+  end  
+
+  def pop 
+    $list.pop
+  end  
+
+  def contains? (item)
+    i = 0
+    toggle = nil
+    while i < $list.length do
+      if $list[i].value === item
+        toggle = true
+      end
+      i += 1
+    end
+    toggle === true ? toggle : false   
+  end  
+
+  def find (value)
+    index = 'nil'
+    $list.each_with_index.map { |node, i| if node.value === value
+    index = i
+  end }
+    return index
+  end  
+
+  def to_s
+    $list.map { |node| puts node }
+  end   
 
 end
 
@@ -19,7 +68,3 @@ class Node
 
   next_node = nil
 end  
-
-list_1 = LinkedList.new
-list_1.append('Nathan')
-puts $list
